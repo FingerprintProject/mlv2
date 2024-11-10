34ec708c87e87eed815146a2ad5adadb9e151180
Nirand Pisutha-Arnond <nnnpooh@gmail.com>
2024-11-10 17:18:18 +0700

    Update

--- docker/docker-entrypoint-initdb.d/02_alter_fp_model.sql
+++ docker/docker-entrypoint-initdb.d/02_alter_fp_model.sql
@@ -1,5 +1,5 @@
 ALTER TABLE fp_models 
-ADD COLUMN class_name varchar(100) NOT NULL DEFAULT 'V1',
+ADD COLUMN details jsonb,
 ADD COLUMN instance_id varchar(100) NOT NULL DEFAULT 'V1';
 ALTER TABLE fp_models 
 ALTER COLUMN class_name SET DEFAULT '',
